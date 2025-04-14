7fa080b8fdd51d14bcebb930b3b6810bc49512a8
Chawketodeh <113772963+Chawketodeh@users.noreply.github.com>
2025-04-14 23:45:01 +0100

    modification ba3d ma 5let

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 export const test = (req, res) => {
     res.json({
-        Message:'Api route is working!',
-    }); 
-};
+      Message: 'Api route is working!',
+    });
+  };
+  
--- api/index.js
+++ api/index.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO)
+  .then(() => {
     console.log('Connected to MongoDB');
-    }).catch((err)=>{
-        console.log(err);
-    })
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+const app = express();
 
-const app = express(); 
+app.use(express.json());
+app.use('/api/user', userRouter); // ✅ this must come before app.listen()
 
-app.listen(3000, ()=> {
-    console.log('Server is running on port 3000!');
-    }
-)
- 
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
