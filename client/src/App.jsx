f7883a0ee7cf25d05bc9ac61c0156dd783c9a256
Chawketodeh <113772963+Chawketodeh@users.noreply.github.com>
2025-04-15 22:34:49 +0100

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Home from './pages/Home';
-import SignIn from './pages/signIn';
+import SignIn from './pages/SignIn';
 import Header from './components/Header';
 
 export default function App() {
