800b0c88027ed1cb88bebe30eac579b9d17b3734
Chawketodeh <113772963+Chawketodeh@users.noreply.github.com>
2025-04-25 16:15:22 +0100

    complete image upload functionality (there was an error in firebase storagebucket and i fix it )  add to that i installed firebase packages

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-estate-40c90.firebaseapp.com",
   projectId: "mern-estate-40c90",
-  storageBucket: "mern-estate-40c90.appspot.com",
+  storageBucket: "mern-estate-40c90.firebasestorage.app",
   messagingSenderId: "453138741027",
   appId: "1:453138741027:web:fddfe21c18d7a4709fbe96"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
